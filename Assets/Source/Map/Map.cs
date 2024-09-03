4ba5f25a60fa497b3b6bfec123c4cbe1b2c30343
ForsakenAginor <juriys2022@mail.ru>
2024-09-03 17:57:25 +0400

    add forest/plains generation with Perlin noise

--- Assets/Source/Map/Map.cs
+++ Assets/Source/Map/Map.cs
@@ -1,21 +1,68 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Map
 {
     private const int Size = 4000;
 
     private readonly byte[] _mapContent = new byte[Size * Size];
+    private readonly float _plainsSizeFactor;
+    private readonly float _forestSpawnFrequency;
 
-    public Map()
+    public Map(float plainsSizeFactor, float forestSpawnFrequency)
     {
-        Tile tile;
+        _plainsSizeFactor = plainsSizeFactor >= 0 && plainsSizeFactor <= 1 ?
+            plainsSizeFactor :
+            throw new ArgumentOutOfRangeException(nameof(plainsSizeFactor));
 
-        for (int i = 0; i < _mapContent.Length; i++)
+        _forestSpawnFrequency = forestSpawnFrequency > 0 ?
+            forestSpawnFrequency :
+            throw new ArgumentOutOfRangeException(nameof(forestSpawnFrequency));
+
+        int index = 0;
+
+        for (int y = 0; y < Size; y++)
         {
-            tile = new Tile(Biom.Forest, TileFlags.None);
-            _mapContent[i] = tile.Content;
+            for (int x = 0; x < Size; x++)
+            {
+                _mapContent[index] = GenerateTile(x, y);
+                index++;
+            }
         }
+        _forestSpawnFrequency = forestSpawnFrequency;
     }
 
     public IEnumerable<byte> MapContent => _mapContent;
+
+    private byte GenerateTile(int x, int y)
+    {
+        float perlinNoiseResult = Mathf.Clamp01(
+            Mathf.PerlinNoise(
+                (float)x / Size * _forestSpawnFrequency,
+                (float)y / Size * _forestSpawnFrequency));
+
+        if (perlinNoiseResult > _plainsSizeFactor)
+            return Encode(Biom.Forest, TileFlags.None);
+        else
+            return Encode(Biom.Plains, TileFlags.None);
+    }
+
+    private byte Encode(Biom biom, TileFlags tileFlag)
+    {
+        const int BiomShiftFactor = 4;
+        const int TileFlagShiftFactor = 0;
+        const int MaxValue = 0xF;
+
+        if ((int)biom > MaxValue || (int)biom < 0)
+            throw new ArgumentOutOfRangeException(nameof(biom));
+
+        if ((int)tileFlag > MaxValue || (int)tileFlag < 0)
+            throw new ArgumentOutOfRangeException(nameof(biom));
+
+        return (byte)(
+            (byte)(((int)biom & MaxValue) << BiomShiftFactor) +
+            (byte)(((int)tileFlag & MaxValue) << TileFlagShiftFactor)
+            );
+    }
 }
--- Assets/Source/Map/Root.cs
+++ Assets/Source/Map/Root.cs
@@ -4,15 +4,19 @@ public class Root : MonoBehaviour
 {
     [SerializeField] private BiomColorSetter _colorSetter;
     [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField][Range(0, 1)] private float _plainsSizeFactor;
+    [SerializeField] private float _forestSpawnFrequency;
 
     private void Awake()
     {
-        Map map = new Map();
-        TextureCreator textureCreator = new(_colorSetter.BiomColorPairs);
+        Debug.Log("");
+        Map map = new (_plainsSizeFactor, _forestSpawnFrequency);
+        TextureCreator textureCreator = new (_colorSetter.BiomColorPairs);
         Texture2D texture = textureCreator.Create(map.MapContent);
         Rect mapRect = new Rect(Vector2.zero, new Vector2(texture.width, texture.height));
         Vector2 pivot = new(0.5f, 0.5f);
         Sprite sprite = Sprite.Create(texture, mapRect, pivot);
         _spriteRenderer.sprite = sprite;
+        Debug.Log(Time.realtimeSinceStartup);
     }
 }
